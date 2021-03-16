ff0309a63b1322014380ef3d9d4d16e4e14d63c5
kbmat <kbmat@hawaii.edu>
2021-03-16 01:43:44 -1000

    Update ex2.js

--- Lab11/ex2.js
+++ Lab11/ex2.js
@@ -1,3 +1,6 @@
+// defining variable attributes
 attributes = "Kimberly;20;MIS";
+// cut string into an array of parts
 parts = attributes.split(';');
+// log the value of parts
 console.log(parts);
